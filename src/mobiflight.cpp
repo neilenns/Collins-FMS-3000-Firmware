83063cbc25dbd6f2c04cbc909facf910c7ea0751
Neil Enns <{ID}+{username}@users.noreply.github.com>
2021-11-23 19:34:03 -0800

    Rip out config stuff

--- src/mobiflight.cpp
+++ src/mobiflight.cpp
@@ -71,8 +71,6 @@ char serial[MEM_LEN_SERIAL] = MOBIFLIGHT_SERIAL;
 char name[MEM_LEN_NAME] = MOBIFLIGHT_NAME;
 const int MEM_LEN_CONFIG = MEMLEN_CONFIG;
 
-char configBuffer[MEM_LEN_CONFIG] = "";
-
 uint16_t configLength = 0;
 boolean configActivated = false;
 
@@ -178,7 +176,6 @@ void attachCommandCallbacks()
 void OnResetBoard()
 {
   MFeeprom.init();
-  configBuffer[0] = '\0';
   generateSerial(false);
   lastCommand = millis();
   loadConfig();
@@ -216,19 +213,7 @@ void generateSerial(bool force)
 
 void loadConfig()
 {
-  MFeeprom.read_block(MEM_OFFSET_CONFIG, configBuffer, MEM_LEN_CONFIG);
-#ifdef DEBUG
-  cmdMessenger.sendCmd(kStatus, F("Restored config"));
-  cmdMessenger.sendCmd(kStatus, configBuffer);
-#endif
-  configLength = strlen(configBuffer);
-  readConfig();
   _activateConfig();
-}
-
-void _storeConfig()
-{
-  MFeeprom.write_block(MEM_OFFSET_CONFIG, configBuffer, MEM_LEN_CONFIG);
 }
 
 void SetPowerSavingMode(bool state)
@@ -602,32 +587,11 @@ void handlerOnAnalogChange(int value, uint8_t pin, const char *name)
  **/
 void OnSetConfig()
 {
-#ifdef DEBUG
-  cmdMessenger.sendCmd(kStatus, F("Setting config start"));
-#endif
   lastCommand = millis();
-  char *cfg = cmdMessenger.readStringArg();
 
-#ifdef FIXED_CONFIG
-  // For fixed configs just pretend that we took the config sent and are ready to take more.
+  // Since this firmware has a fixed config just report back a length to make
+  // the desktop app happy.
   cmdMessenger.sendCmd(kStatus, configLength);
-  return;
-#endif
-
-  uint8_t cfgLen = strlen(cfg);
-  uint16_t bufferSize = MEM_LEN_CONFIG - (configLength + cfgLen);
-
-  if (bufferSize > 1)
-  {
-    memcpy(&configBuffer[configLength], cfg, bufferSize);
-    configLength += cfgLen;
-    cmdMessenger.sendCmd(kStatus, configLength);
-  }
-  else
-    cmdMessenger.sendCmd(kStatus, -1);
-#ifdef DEBUG
-  cmdMessenger.sendCmd(kStatus, F("Setting config end"));
-#endif
 }
 
 void OnResetConfig()
@@ -642,7 +606,6 @@ void OnSaveConfig()
 
 void OnActivateConfig()
 {
-  readConfig();
   _activateConfig();
 }
 
@@ -650,138 +613,6 @@ void _activateConfig()
 {
   configActivated = true;
   cmdMessenger.sendCmd(kConfigActivated, F("OK"));
-}
-
-void readConfig()
-{
-  if (configLength == 0)
-    return;
-  char *p = NULL;
-
-  char *command = strtok_r(configBuffer, ".", &p);
-  char *params[6];
-  if (*command == 0)
-    return;
-
-  do
-  {
-    switch (atoi(command))
-    {
-    case kTypeButton:
-      params[0] = strtok_r(NULL, ".", &p); // pin
-      params[1] = strtok_r(NULL, ":", &p); // name
-      AddButton(atoi(params[0]), params[1]);
-      break;
-
-    case kTypeOutput:
-      params[0] = strtok_r(NULL, ".", &p); // pin
-      params[1] = strtok_r(NULL, ":", &p); // Name
-      AddOutput(atoi(params[0]), params[1]);
-      break;
-
-    case kTypeLedSegment:
-      params[0] = strtok_r(NULL, ".", &p); // pin Data
-      params[1] = strtok_r(NULL, ".", &p); // pin Cs
-      params[2] = strtok_r(NULL, ".", &p); // pin Clk
-      params[3] = strtok_r(NULL, ".", &p); // brightness
-      params[4] = strtok_r(NULL, ".", &p); // numModules
-      params[5] = strtok_r(NULL, ":", &p); // Name
-                                           // int dataPin, int clkPin, int csPin, int numDevices, int brightness
-#if MF_SEGMENT_SUPPORT == 1
-      AddLedSegment(atoi(params[0]), atoi(params[1]), atoi(params[2]), atoi(params[4]), atoi(params[3]));
-#endif
-      break;
-
-    case kTypeStepperDeprecated:
-      // this is for backwards compatibility
-      params[0] = strtok_r(NULL, ".", &p); // pin1
-      params[1] = strtok_r(NULL, ".", &p); // pin2
-      params[2] = strtok_r(NULL, ".", &p); // pin3
-      params[3] = strtok_r(NULL, ".", &p); // pin4
-      params[4] = strtok_r(NULL, ".", &p); // btnPin1
-      params[5] = strtok_r(NULL, ":", &p); // Name
-#if MF_STEPPER_SUPPORT == 1
-      AddStepper(atoi(params[0]), atoi(params[1]), atoi(params[2]), atoi(params[3]), 0);
-#endif
-      break;
-
-    case kTypeStepper:
-      // AddStepper(int pin1, int pin2, int pin3, int pin4)
-      params[0] = strtok_r(NULL, ".", &p); // pin1
-      params[1] = strtok_r(NULL, ".", &p); // pin2
-      params[2] = strtok_r(NULL, ".", &p); // pin3
-      params[3] = strtok_r(NULL, ".", &p); // pin4
-      params[4] = strtok_r(NULL, ".", &p); // btnPin1
-      params[5] = strtok_r(NULL, ":", &p); // Name
-#if MF_STEPPER_SUPPORT == 1
-      AddStepper(atoi(params[0]), atoi(params[1]), atoi(params[2]), atoi(params[3]), atoi(params[4]));
-#endif
-      break;
-
-    case kTypeServo:
-      // AddServo(int pin)
-      params[0] = strtok_r(NULL, ".", &p); // pin1
-      params[1] = strtok_r(NULL, ":", &p); // Name
-#if MF_SERVO_SUPPORT == 1
-      AddServo(atoi(params[0]));
-#endif
-      break;
-
-    case kTypeEncoderSingleDetent:
-      // AddEncoder(uint8_t pin1 = 1, uint8_t pin2 = 2, uint8_t encoder_type = 0, String name = "Encoder")
-      params[0] = strtok_r(NULL, ".", &p); // pin1
-      params[1] = strtok_r(NULL, ".", &p); // pin2
-      params[2] = strtok_r(NULL, ":", &p); // Name
-      AddEncoder(atoi(params[0]), atoi(params[1]), 0, params[2]);
-      break;
-
-    case kTypeEncoder:
-      // AddEncoder(uint8_t pin1 = 1, uint8_t pin2 = 2, uint8_t encoder_type = 0, String name = "Encoder")
-      params[0] = strtok_r(NULL, ".", &p); // pin1
-      params[1] = strtok_r(NULL, ".", &p); // pin2
-      params[2] = strtok_r(NULL, ".", &p); // encoder_type
-      params[3] = strtok_r(NULL, ":", &p); // Name
-      AddEncoder(atoi(params[0]), atoi(params[1]), atoi(params[2]), params[3]);
-      break;
-
-    case kTypeLcdDisplayI2C:
-      // AddEncoder(uint8_t address = 0x24, uint8_t cols = 16, lines = 2, String name = "Lcd")
-      params[0] = strtok_r(NULL, ".", &p); // address
-      params[1] = strtok_r(NULL, ".", &p); // cols
-      params[2] = strtok_r(NULL, ".", &p); // lines
-      params[3] = strtok_r(NULL, ":", &p); // Name
-#if MF_LCD_SUPPORT == 1
-      AddLcdDisplay(atoi(params[0]), atoi(params[1]), atoi(params[2]), params[3]);
-#endif
-      break;
-
-    case kTypeAnalogInput:
-      params[0] = strtok_r(NULL, ".", &p); // pin
-      params[1] = strtok_r(NULL, ".", &p); // sensitivity
-      params[2] = strtok_r(NULL, ":", &p); // name
-#if MF_ANALOG_SUPPORT == 1
-      AddAnalog(atoi(params[0]), params[2], atoi(params[1]));
-#endif
-      break;
-
-    case kShiftRegister:
-      params[0] = strtok_r(NULL, ".", &p); // pin latch
-      params[1] = strtok_r(NULL, ".", &p); // pin clock
-      params[2] = strtok_r(NULL, ".", &p); // pin data
-      params[3] = strtok_r(NULL, ".", &p); // number of daisy chained modules
-      params[4] = strtok_r(NULL, ":", &p); // name
-#if MF_SHIFTER_SUPPORT == 1
-      AddShifter(atoi(params[0]), atoi(params[1]), atoi(params[2]), atoi(params[3]), params[4]);
-#endif
-      break;
-
-    default:
-      // read to the end of the current command which is
-      // apparently not understood
-      params[0] = strtok_r(NULL, ":", &p); // read to end of unknown command
-    }
-    command = strtok_r(NULL, ".", &p);
-  } while (command != NULL);
 }
 
 // Called when a received command has no attached function
