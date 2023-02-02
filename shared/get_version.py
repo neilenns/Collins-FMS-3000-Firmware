a7c7ed9fd39543a1803042837d0bfdcc5feacef7
Neil Enns <{ID}+{username}@users.noreply.github.com>
2023-02-02 13:43:26 -0800

    Fix number used in display name

--- shared/get_version.py
+++ shared/get_version.py
@@ -12,12 +12,13 @@ raw_board_number = os.environ.get('BOARD_NUMBER', "1")
 # Convert board_number to origin 0 so it can be used to index into board_position_name.
 board_number = int(raw_board_number) - 1
 
-print(f'Using version {firmware_version} position {board_position_name[board_number]} ({raw_board_number}) for the build')
+print(
+    f'Using version {firmware_version} position {board_position_name[board_number]} ({raw_board_number}) for the build')
 
 # Append the version to the build defines so it gets baked into the firmware
 env.Append(CPPDEFINES=[
     f'BUILD_VERSION={firmware_version}',
-    f'MOBIFLIGHT_NAME=FMS 3000 - {board_number}'
+    f'MOBIFLIGHT_NAME=FMS 3000 - {raw_board_number}'
 ])
 
 # Set the output filename to the name of the board and the version
