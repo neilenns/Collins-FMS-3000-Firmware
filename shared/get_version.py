777d6917714a58ef10dfb5db08017da2f5b0aa4d
Neil <neile@live.com>
2023-02-02 06:35:50 -0800

    Fix position number and debug string

--- shared/get_version.py
+++ shared/get_version.py
@@ -7,12 +7,12 @@ board_position_name = ["pilot", "co-pilot"]
 
 # Get the version number from the build environment.
 firmware_version = os.environ.get('VERSION', "2.2.0")
-board_number = os.environ.get('BOARD_NUMBER', 1)
+raw_board_number = os.environ.get('BOARD_NUMBER', "1")
 
 # Convert board_number to origin 0 so it can be used to index into board_position_name.
-board_number -= 1
+board_number = int(raw_board_number) - 1
 
-print(f'Using version {firmware_version} position {board_position_name} ({board_number}) for the build')
+print(f'Using version {firmware_version} position {board_position_name[board_number]} ({raw_board_number}) for the build')
 
 # Append the version to the build defines so it gets baked into the firmware
 env.Append(CPPDEFINES=[
